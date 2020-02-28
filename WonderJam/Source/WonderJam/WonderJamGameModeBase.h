103de89f61a552840b6e1ce2e5d90693daa5204b
Yuukini <garcia.florian74@gmail.com>
2020-02-28 14:30:58 -0500

    Initial commit
    push avec starter content d'un blank project

---
+++ WonderJam/Source/WonderJam/WonderJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WonderJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WONDERJAM_API AWonderJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
